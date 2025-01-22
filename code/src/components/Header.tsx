3f2b5e4469aee639df8910f185438f1e2a2b976f
cglavin50 <cooperglavin@gmail.com>
2025-01-21 20:53:17 -0500

    Initial commit of page

---
+++ code/src/components/Header.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Header.css';
+
+const Header: React.FC = () => {
+  return (
+    <header className="header">
+      <div className="header-content">
+        <h1>Hi, I'm Cooper Glavin</h1>
+        <h2>I'm a Junior Software Developer</h2>
+      </div>
+    </header>
+  );
+};
+
+export default Header; 
